c77117755353e4943bf7583ada0e0a7aae90cbc0
EgoriusRus <54712243+EgoriusRus@users.noreply.github.com>
2022-04-01 15:02:53 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# gc-function-seo
+# Firebase function for seo meta tags
+
+## Step 1
+Deploy this code to google cloud function
+https://cloud.google.com/functions/docs/deploying
+
+Set `seo` as entry point
+
+## Step 2
+Setup `API_URL` ENV variable
+https://cloud.google.com/functions/docs/configuring/env-var
+
+Function call `API_URL/seo` endpoint that should return json object
+```
+{
+    "title": "page title",
+    "desc": "page description",
+    "image": "page thumbnail"
+}
+```
+
+## Step 3
+Update `firebase.json`
+Set rewrite on routes where you would like to get dynamic tags
+```
+"rewrites": [
+  {
+    "source": "**/event/**",
+    "function": "yourFunctionName"
+  },
+]
---
+++ index.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const functions = require("firebase-functions");
+const ax = require("axios");
+
+const axios = ax.create({
+  baseURL: `${process.env.API_URL}`,
+  timeout: 1500,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error)
+);
+
+exports.seo = functions.https.onRequest(async (req, res) => {
+  const websiteURL = `${req.headers["x-forwarded-proto"]}://${req.headers["x-forwarded-host"]}`;
+
+  const { data: html } = await ax.get(websiteURL);
+
+  const { data: meta } = await axios.post(`/seo`, {
+    url: websiteURL + req.url,
+  });
+
+  html = getFilledWithTags(html, meta);
+  res.send(html);
+});
+
+const getFilledWithTags = (html, { title, desc, image }) => {
+  return html.replace(
+    `<head>`,
+    `<head>
+          <meta property="og:title" content="${title}" />
+          <meta property="og:description" content="${desc}" />
+          <meta property="og:image" content=${image} />
+          <meta property="og:url" content="${websiteURL + req.url}" />
+          <meta name="twitter:title" content="${title}" />
+          <meta name="twitter:description" content="${desc}" />
+          <meta name="twitter:image" content=${image} />
+          <title>${title}</title>
+        `
+  );
+};
